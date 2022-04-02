5f227c73a866a52e2e8a31074170ad36f11d0b2a
Juan Endl <juanendl2021@hotmail.com>
2022-04-02 03:58:13 -0300

    add footer css

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,170 +1,115 @@
 //todavia a preparar
 
-import React from 'react';
+import logofooter from '../assets/iconos/logofooter2.png';
+import github from '../assets/iconos/github.png';
+import linkedin from '../assets/iconos/linkedin.png';
+import './Footer.css';
 
-const Footer = (props) => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { VscGithub } from 'react-icons/vsc';
+
+
+
+const Footer = () => {
 	return (
 		<>
-			<footer class="text-gray-600 body-font">
-				<div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-					<div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-						<a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-								viewBox="0 0 24 24"
-							>
-								<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-							</svg>
-							<span class="ml-3 text-xl">Tailblocks</span>
+			<footer className="bgfooterColor text-gray-600 body-font">
+				<div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+					<div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+						<a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+							<Link to="/">
+								<img className="logoFotter" src={logofooter} alt="logofooter" />
+							</Link>
+							<span className="ml-3 text-xl">Virtual Computer</span>
 						</a>
-						<p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+						<p className="mt-2 text-sm text-gray-500 ml-8">Lider en ventas</p>
 					</div>
-					<div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-						<div class="lg:w-1/4 md:w-1/2 w-full px-4">
-							<h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-							<nav class="list-none mb-10">
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">First Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Second Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Third Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-								</li>
+					<div className="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+						<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+							<h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ASUS</h2>
+							<nav className="list-none mb-10">
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">First Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Second Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Third Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Fourth Link</p>
+							</li>
 							</nav>
 						</div>
-						<div class="lg:w-1/4 md:w-1/2 w-full px-4">
-							<h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-							<nav class="list-none mb-10">
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">First Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Second Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Third Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-								</li>
+						<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+							<h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">RYZER</h2>
+							<nav className="list-none mb-10">
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">First Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Second Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Third Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Fourth Link</p>
+							</li>
 							</nav>
 						</div>
-						<div class="lg:w-1/4 md:w-1/2 w-full px-4">
-							<h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-							<nav class="list-none mb-10">
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">First Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Second Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Third Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-								</li>
+						<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+							<h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">COOLER MASTER</h2>
+							<nav className="list-none mb-10">
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">First Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Second Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Third Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Fourth Link</p>
+							</li>
 							</nav>
 						</div>
-						<div class="lg:w-1/4 md:w-1/2 w-full px-4">
-							<h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-							<nav class="list-none mb-10">
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">First Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Second Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Third Link</a>
-								</li>
-								<li>
-									<a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-								</li>
+						<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+							<h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">NVIDIA</h2>
+							<nav className="list-none mb-10">
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">First Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Second Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Third Link</p>
+							</li>
+							<li>
+								<p class="text-gray-600 hover:text-gray-800">Fourth Link</p>
+							</li>
 							</nav>
 						</div>
 					</div>
 				</div>
-				<div class="bg-gray-100">
-					<div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-						<p class="text-gray-500 text-sm text-center sm:text-left">
-							© 2020 Tailblocks —
-							<a
-								href="https://twitter.com/knyttneve"
-								rel="noopener noreferrer"
-								class="text-gray-600 ml-1"
-								target="_blank"
-							>
-								@knyttneve
-							</a>
+				<div className="bgfooterColorTwo bg-gray-100">
+					<div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+						<p className="text-white mt-">
+							© 2022 Camada 25445 --
+							<a href="https://github.com/JuanEndl/React-Js-camada-25445" rel="noopener noreferrer" className="text-white" target="_blank"> @GitHub </a>
 						</p>
-						<span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-							<a class="text-gray-500">
-								<svg
-									fill="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									class="w-5 h-5"
-									viewBox="0 0 24 24"
-								>
-									<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-								</svg>
-							</a>
-							<a class="ml-3 text-gray-500">
-								<svg
-									fill="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									class="w-5 h-5"
-									viewBox="0 0 24 24"
-								>
-									<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-								</svg>
-							</a>
-							<a class="ml-3 text-gray-500">
-								<svg
-									fill="none"
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									class="w-5 h-5"
-									viewBox="0 0 24 24"
-								>
-									<rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
-									<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
-								</svg>
-							</a>
-							<a class="ml-3 text-gray-500">
-								<svg
-									fill="currentColor"
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="0"
-									class="w-5 h-5"
-									viewBox="0 0 24 24"
-								>
-									<path
-										stroke="none"
-										d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-									/>
-									<circle cx="4" cy="4" r="2" stroke="none" />
-								</svg>
-							</a>
+						<span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+						<a href="https://github.com/JuanEndl?tab=repositories" rel="noopener noreferrer" className="text-white" target="_blank">
+							<img className='iconInfo' src={github} alt="github"/>
+						</a>
+						<a href="https://www.linkedin.com/in/juan-cruz-endl" rel="noopener noreferrer" className="text-white" target="_blank">
+							<img className='iconInfo ml-3' src={linkedin} alt="github"/>
+						</a>
 						</span>
 					</div>
 				</div>
